8a2a58c8175aea75acea22a8b743290a29e3d9d6
khannavikas <mail2vikas03@gmail.com>
2021-01-29 16:44:29 +0530

    Update: README.md notebooks

--- notebooks/cs2-initialize-volume-mounting.scala
+++ notebooks/cs2-initialize-volume-mounting.scala
@@ -3,12 +3,12 @@ import scala.util.control._
 
 // COMMAND ----------
 
-al configs = Map(
+ val configs = Map(
   "fs.azure.account.auth.type" -> "OAuth",
   "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-  "fs.azure.account.oauth2.client.id" -> "f84616aa-d6be-478e-b2fc-9ef7c9ca078b",
-  "fs.azure.account.oauth2.client.secret" -> dbutils.secrets.get(scope = "training-scope", key = "appsecret"),
-  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/381a10df-8e85-43db-86e1-8893b075b027/oauth2/token")
+  "fs.azure.account.oauth2.client.id" -> "29f55893-424f-4030-b8f6-2d4b7f20d7b0",
+  "fs.azure.account.oauth2.client.secret" -> "1Rb-_2yhR2NOMcs.evKs5q1d9m~1b20GI.",
+  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/82e453a4-e35f-4a61-8902-1e5887e94115/oauth2/token")
 
 
 // COMMAND ----------
